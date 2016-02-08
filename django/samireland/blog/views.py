84b7e56e6ce438156290cd75f6e1b64495fd0c78
Sam Ireland <sam.ireland.uk@gmail.com>
2016-02-08 15:47:03 +0000

    Change django public app's name to blog

--- django/samireland/blog/views.py
+++ django/samireland/blog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render_to_response("public/home.html")
+    return render_to_response("blog/home.html")
